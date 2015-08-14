6fd86e09ea45c269c602e2a5e8d021698734e1c1
dannygallant <danny.gallant@att.net>
2015-08-14 09:59:25 -0500

    Create answer array
    Created array holding answers to questions.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,58 @@
 $(document).ready(function() {
     //quiz questions
     var questions = [{
-        choices: ["Fender Stratocaster", "Gibson Les Paul", "Gibson ES-335", "Fender Jazzmaster"],
-        questionNum : 0,
-        answer : 3,
-        fact: "The Fender Stratocaster is one of the world's most popular guitars."
+        guitarChoices: ["Fender Stratocaster", "Gibson Les Paul", "Gibson ES-335", "Fender Jazzmaster", "Epiphone Casino"],
+        artistChoices: ["Jimmy Page", "Keith Richards", "James Hetfield", "Eric Clapton", "Brian Setzer"],
+        pic: "img/strat_border_smaller.gif",
+    	questionNum: 0,
+        guitarAnswer: 0,
+        aristAnswer: 3,
+        info: "The Fender Stratocaster is one of the world's most popular guitars. It's used in many musical genres. Eric Clapton transitioned from Gibson guitars to the Stratocaster in late 1969.",
         },
+        {
+        guitarChoices: ["Gibson SG", "Fender Telecaster", "Gibson Les Paul", "Gretsch White Falcon", "PRS Core"],
+        artistChoices: ["Stevie Ray Vaughan", "Billy Gibbons", "Jeff Beck", "Howlin' Wolf", "Paul Stanley"],
+        pic: "img/les_paul_border_smaller.gif",
+    	questionNum: 1,
+        guitarAnswer: 2,
+        aristAnswer: 1,
+        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        },
+		{
+        guitarChoices: ["Gibson ES-335", "Danelectro Shorthorn" ,"Fender Telecaster", "Fender Jaguar", "Gretsch White Falcon"],
+        artistChoices: ["Jimmie Vaughan", "Jimi Hendrix", "Colin James", "Muddy Waters", "Johnny Winter"],
+        pic: "img/les_paul_border_smaller.gif",
+        questionNum: 2,
+        guitarAnswer: 2,
+        aristAnswer: 1,
+        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        },
+		{
+        guitarChoices: ["Fender Mustang", "Gibson Hummingbird" ,"Ibanez ARZ", "PRS Core", "Taylor 716e"],
+        artistChoices: ["James Taylor", "Jim Croce", "Slash", ""],
+        pic: "img/les_paul_border_smaller.gif",
+        questionNum: 3,
+        guitarAnswer: 2,
+        aristAnswer: 1,
+        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        },
+		{
+        guitarChoices: ["Fender Mustang", "Gibson Hummingbird" ,"Ibanez ARZ", "PRS Core", "Taylor 716e"],
+        artistChoices: ["Jeff Beck", "Tab Benoit", "Eddie Van Halen", "Steve Stevens", "Jennifer Batten"],
+        pic: "img/les_paul_border_smaller.gif",
+        questionNum: 4,
+        guitarAnswer: 2,
+        aristAnswer: 1,
+        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        }]
 
 
-}
+} // document ready end bracket
+
+  
+
+
+
+
+
+
