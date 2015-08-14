db46a90e98d5db910f6f375e5268a0ce087e0c31
dannygallant <danny.gallant@att.net>
2015-08-13 20:54:34 -0500

    Repository setup
    Setup directories & files. Copied file the static quiz app repo.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //quiz questions
+    var questions = [{
+        choices: ["Fender Stratocaster", "Gibson Les Paul", "Gibson ES-335", "Fender Jazzmaster"],
+        questionNum : 0,
+        answer : 3,
+        fact: "The Fender Stratocaster is one of the world's most popular guitars."
+        },
+
+
+}
