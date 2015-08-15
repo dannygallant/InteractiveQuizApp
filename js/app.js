5134a4d9a86b8a862f5ae9f6da875ed0de277305
dannygallant <danny.gallant@att.net>
2015-08-15 14:42:34 -0500

    Content & photo changes
    Swapped out a few photos and modified answer & info content.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     	questionNum: 0,
         guitarAnswer: 0,
         artistAnswer: 3,
-        info: "The Fender Stratocaster is one of the world's most popular guitars. It's used in many musical genres. Eric Clapton transitioned from Gibson guitars to the Stratocaster in late 1969.",
+        info: "The Fender Stratocaster is one of the world's most popular guitars. Eric Clapton transitioned from Gibson guitars to the Stratocaster in late 1969.",
         },
         {
         guitarChoices: ["Gibson SG", "Fender Telecaster", "Gibson Les Paul", "Gretsch White Falcon", "PRS Core"],
@@ -16,34 +16,34 @@ $(document).ready(function() {
     	questionNum: 1,
         guitarAnswer: 2,
         artistAnswer: 1,
-        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        info: "The Gibson Les Paul is one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
         },
 		{
         guitarChoices: ["Gibson ES-335", "Danelectro Shorthorn" ,"Fender Telecaster", "Fender Jaguar", "Gretsch White Falcon"],
         artistChoices: ["Jimmie Vaughan", "Jimi Hendrix", "Colin James", "Muddy Waters", "Johnny Winter"],
-        pic: "img/les_paul_border_smaller.gif",
+        pic: "img/tele_smaller.gif",
         questionNum: 2,
         guitarAnswer: 2,
-        artistAnswer: 1,
-        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        artistAnswer: 3,
+        info: "The Fender Telecaster was the world's first commercially available solid body, single-cutaway guitar. Muddy Waters, along with countless other greats, was an early adopter of the Tele.",
         },
 		{
-        guitarChoices: ["Fender Mustang", "Gibson Hummingbird" ,"Ibanez ARZ", "PRS Core", "Taylor 716e"],
-        artistChoices: ["James Taylor", "Jim Croce", "Slash", ""],
-        pic: "img/les_paul_border_smaller.gif",
+        guitarChoices: ["Fender Mustang", "Gibson Hummingbird" ,"Ibanez ARZ", "PRS DGT", "Taylor 716e"],
+        artistChoices: ["James Taylor", "Jim Croce", "Slash", "Pete Townsend", "David Grissom"],
+        pic: "img/PRS_DGT_smaller.gif",
         questionNum: 3,
-        guitarAnswer: 2,
-        artistAnswer: 1,
-        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        guitarAnswer: 3,
+        artistAnswer: 4,
+        info: "The DGT is from PRS' artist series and is spec'd by David Grissom's personal preference. Many consider PRS guitars to be a meeting point between a Strat and a Les Paul.",
         },
 		{
         guitarChoices: ["Fender Mustang", "Gibson Hummingbird" ,"Ibanez ARZ", "PRS Core", "Taylor 716e"],
-        artistChoices: ["Jeff Beck", "Tab Benoit", "Eddie Van Halen", "Steve Stevens", "Jennifer Batten"],
-        pic: "img/les_paul_border_smaller.gif",
+        artistChoices: ["Jeff Beck", "Tab Benoit", "Eddie Van Halen", "John McLaughlin", "Jennifer Batten"],
+        pic: "img/Gibson_Hummingbird_smaller.gif",
         questionNum: 4,
-        guitarAnswer: 2,
-        artistAnswer: 1,
-        info: "The Gibson Les Paul is a solid body electric guitar that was first sold by the Gibson Guitar Corporation in 1952.[1] The Les Paul was designed by guitarist/inventor Les Paul with the assistance of Ted McCarty and his team. It's one of the most recognizable guitar in the world and is a favorite of countless guitarists, including Billy Gibbons, Slash and Warren Haynes.",
+        guitarAnswer: 1,
+        artistAnswer: 3,
+        info: "Since it's introduction in 1960, the Gibson Hummingbird become a go-to acoustic for many artist, including John McLaughlin. It can be heard on countless recording spanning more than 50 years.",
         }]
 
 
@@ -82,22 +82,20 @@ $("#next_question").on("click", "#nextButton", function () {
 	currentQuestion++;
 	populateQuestionaire(currentQuestion);
 // advance to the next question values in the array
-
+	updateResultsCounter();
 	$("#submit_answer").css("display","inline");
-
-    
-//  add logic to change questions, photo, etc to the next element in the array. 
-//  be sure to check that we are not at the end of the list of questions.
-
 });
 
 function populateQuestionaire() {
-	var newQuestion = '<li></li><li><span class="question"><h4>Which guitar is this?</h4></span><br><div id="answerChoicesGit"><input type="radio" name="option" class="option" value="0"><span class="answer"> ' + questions[currentQuestion].guitarChoices[0]+ '</span><br><input type="radio" name="option" class="option" value="1"><span class="answer"> ' +questions[currentQuestion].guitarChoices[1]+ '</span><br><input type="radio" name="option" class="option" value="2"><span class="answer"> ' +questions[currentQuestion].guitarChoices[2]+ '</span><br><input type="radio" name="option" class="option" value="3"><span class="answer"> ' +questions[currentQuestion].guitarChoices[3]+ '</span><br><input type="radio" name="option" class="option" value="4"><span class="answer"> ' +questions[currentQuestion].guitarChoices[4]+ '</span><br></div></li><li><center><img class="th" src=" ' +questions[currentQuestion].pic+ ' "></center></li><li><span class="question"><h4>Who plays this guitar?</h4></span><br><div id="answerChoicesArt"><input type="radio" name="option2" class="option2" value="0"><span class="answer"> ' +questions[currentQuestion].artistChoices[0]+ '</span><br><input type="radio" name="option2" class="option2" value="1"><span class="answer"> ' +questions[currentQuestion].artistChoices[1]+ '</span><br><input type="radio" name="option2" class="option2" value="2"><span class="answer"> ' +questions[currentQuestion].artistChoices[2]+ '</span><br><input type="radio" name="option2" class="option2" value="3"><span class="answer"> ' +questions[currentQuestion].artistChoices[3]+ '</span><br><input type="radio" name="option2" class="option2" value="4"><span class="answer"> ' +questions[currentQuestion].artistChoices[4]+ '</span><br></div></li><li></li>';
-
+	var newQuestion = '<li></li><li><span class="question"><h4>Which guitar is this?</h4></span><br><div id="answerChoicesGit"><input type="radio" name="option" class="option" value="0"><span class="answer"> ' +questions[currentQuestion].guitarChoices[0]+ '</span><br><input type="radio" name="option" class="option" value="1"><span class="answer"> ' +questions[currentQuestion].guitarChoices[1]+ '</span><br><input type="radio" name="option" class="option" value="2"><span class="answer"> ' +questions[currentQuestion].guitarChoices[2]+ '</span><br><input type="radio" name="option" class="option" value="3"><span class="answer"> ' +questions[currentQuestion].guitarChoices[3]+ '</span><br><input type="radio" name="option" class="option" value="4"><span class="answer"> ' +questions[currentQuestion].guitarChoices[4]+ '</span><br></div></li><li><center><img class="th" src=" ' +questions[currentQuestion].pic+ ' "></center></li><li><span class="question"><h4>Who plays this guitar?</h4></span><br><div id="answerChoicesArt"><input type="radio" name="option2" class="option2" value="0"><span class="answer"> ' +questions[currentQuestion].artistChoices[0]+ '</span><br><input type="radio" name="option2" class="option2" value="1"><span class="answer"> ' +questions[currentQuestion].artistChoices[1]+ '</span><br><input type="radio" name="option2" class="option2" value="2"><span class="answer"> ' +questions[currentQuestion].artistChoices[2]+ '</span><br><input type="radio" name="option2" class="option2" value="3"><span class="answer"> ' +questions[currentQuestion].artistChoices[3]+ '</span><br><input type="radio" name="option2" class="option2" value="4"><span class="answer"> ' +questions[currentQuestion].artistChoices[4]+ '</span><br></div></li><li></li>';
 	$("#questionContainer").html(newQuestion);
-
 }
 
+function displayInfo() {
+	var newInfo = '<p>' +questions[currentQuestion].info+ '</p>';
+	$("#info").html(newInfo);
+	$("#info").css("display", "inline");
+}
 
 function updateCounterGuitar() {
 	var answerGit = $("input:radio[name='option']:checked").val();
@@ -126,6 +124,16 @@ function updateCounterArtist() {
 
     }
 
+// function populateInfo() {
+// 	var newInfo = '<p>' +questions[currentQuestion].info+ '</p>';
+// 	$("#info").html(newInfo);
+
+// }
+
+function updateResultsCounter() {
+	var score = '<center><p>' +"Correct Guitars:"+correctGitCounter+ '</p></center><center><p> ' +"Correct Artist:" +correctArtCounter+ '</p></center>';
+	$("#results").html(score);
+}
 
 // =====  used for testing. Don't need to loop through and use if statement with radios  =====
 // function updateCounterGuitar() {
@@ -148,10 +156,7 @@ function updateCounterArtist() {
 // };
 
 
-function displayInfo() {
-	$("#info").css("display", "inline")
 
-}
 	
 
 
